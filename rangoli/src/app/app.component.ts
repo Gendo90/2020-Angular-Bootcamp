c81ff4b735ea419e0b398675b1d317bbf13e8f60
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-02-20 07:29:05 -0800

    feat: Link up API call to get different rangolis depending on user input to size

--- rangoli/src/app/app.component.ts
+++ rangoli/src/app/app.component.ts
@@ -10,10 +10,10 @@ export class AppComponent {
     CSS_COLOR_NAMES = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"];
 
     //map that contains characters seen already and their assigned color
-    seen_chars = new Map()
+    seen_chars = new Map();
 
     //initial rangoli
-    first_state = ['--------e--------',
+    rangoli_state = ['--------e--------',
                       '------e-d-e------',
                       '----e-d-c-d-e----',
                       '--e-d-c-b-c-d-e--',
@@ -21,5 +21,31 @@ export class AppComponent {
                       '--e-d-c-b-c-d-e--',
                       '----e-d-c-d-e----',
                       '------e-d-e------',
-                      '--------e--------']
+                      '--------e--------'];
+
+    //rangoli background color
+    background_color = "white";
+
+    //size of the rangoli pattern
+    size = 10;
+
+    //url to get the new rangoli pattern
+    url = 'https://wayscript.com/api?';
+
+    updateBackground(color: string) {
+        this.background_color = color;
+        }
+
+
+    getRangoli() {
+        fetch(`https://wayscript.com/api?api_key=HRLP2pxChqSxV1uUFCnPeUo7OmXMTH0OePNsvb7OPww&program_id=8192&variables=${this.size}`
+        ).then(response => {
+            return response.json()
+        })
+        .then(json_result => {
+            console.log(json_result);
+            // console.log(json_result.Result.body.results);
+            this.rangoli_state = json_result.Result.body.results;
+            return this.rangoli_state;
+    });}
 }
