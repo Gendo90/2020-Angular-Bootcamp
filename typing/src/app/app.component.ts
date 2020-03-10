bb8c243d23c0c326f19710ed32154d356e6f6f22
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-03-10 16:40:36 -0700

    feat: Add paragraph(s) modes to the typing app
    Note: Still need to put a "start" button or intro screen, and then
    time the user and collect statistics on their typing (speed &
    accuracy) - maybe have a running timer, current accuracy, and then
    a popup that gives their final statistics

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -10,18 +10,38 @@ export class AppComponent {
   randomText = lorem.sentence();
   inputText = '';
   show = true;
+  textType = "sentence";
+
+  getSentence() {
+      this.randomText = lorem.sentence();
+      this.inputText = '';
+      this.textType = "sentence";
+  }
+
+  getParagraph() {
+      this.randomText = lorem.paragraph();
+      this.inputText = '';
+      this.textType = "paragraph";
+  }
+
+  getParagraphs() {
+      this.randomText = lorem.paragraphs();
+      this.inputText = '';
+      this.textType = "paragraphs";
+  }
 
   getInputText(e: any) {
       let text = e.target.value;
-      console.log(e)
-      console.log(e.inputType)
+      //prohibit pasting
       if (e.inputType === "insertFromPaste") {
             e.preventDefault();
             e.target.value = this.inputText;
         }
+      //only add one letter at a time!
       else if(text.length === this.inputText.length+1) {
           this.inputText = text;
       }
+      //allow multiple character deletions
       else if(text.length <= this.inputText.length-1) {
           this.inputText = text;
       }
