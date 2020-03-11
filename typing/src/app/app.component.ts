31f6e6858ab18dc893376e5f6775befab11aae88
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-03-11 16:01:05 -0700

    feat: Get app functional, with results for typing tests shown for one or more paragraphs (need to bug hunt that feature though)
    Note: Careful with the paragraphs - HTML removes some whitespaces, and
    manipulating the strings can be tricky. Still need to add accuracy
    (different mode that does not need to be 100% accurate to finish), and
    make the stats popup on completion (maybe), and definitely clear the
    textarea when generating new paragraphs or switching between one or
    more paragraph modes. Maybe add some signals to the user that
    formatting matters (cannot miss newlines)

--- typing/src/app/app.component.ts
+++ typing/src/app/app.component.ts
@@ -8,26 +8,86 @@ import { lorem } from "faker";
 })
 export class AppComponent {
   randomText = lorem.sentence();
+  randomTextArr = [this.randomText];
   inputText = '';
   show = true;
   textType = "sentence";
+  currentTime;
+  wpm;
+  timeElapsedMin;
+  timeElapsedSec;
 
   getSentence() {
+      this.randomTextArr = [];
       this.randomText = lorem.sentence();
+      this.randomTextArr.push(this.randomText);
       this.inputText = '';
       this.textType = "sentence";
   }
 
   getParagraph() {
+      this.randomTextArr = [];
       this.randomText = lorem.paragraph();
+      this.randomTextArr.push(this.randomText);
       this.inputText = '';
       this.textType = "paragraph";
   }
 
   getParagraphs() {
-      this.randomText = lorem.paragraphs();
+      this.randomTextArr = [];
+      this.randomText = "";
+      let test = lorem.paragraphs();
+      //remove the carriage returns (left over from the paragraph generator)
+      while (test.includes("\r")) {
+          test = test.replace('\r', "");
+      }
+      let test2 = test.split('\n');
+      for (let i = 0; i<test2.length; i++) {
+          if(i!=0) {
+              //remove the leading space, took a while to find it!
+              test2[i] = test2[i].replace(" ", "");
+          }
+          if(i<test2.length-1) {
+              this.randomText+= (test2[i] + '\n\n');
+              this.randomTextArr.push((test2[i]+"\n\n"));
+          }
+          else {
+              this.randomText+=test2[i]
+              this.randomTextArr.push((test2[i]));
+          }
+      }
       this.inputText = '';
       this.textType = "paragraphs";
+
+      console.log(this.randomText)
+      console.log(this.randomTextArr)
+  }
+
+  getTime() {
+      return new Date();
+  }
+
+  startTest() {
+      this.getParagraphs();
+      this.currentTime = this.getTime();
+  }
+
+  startPracticeTest() {
+      this.getParagraph();
+      this.currentTime = this.getTime();
+  }
+
+  endTest() {
+      let timeElapsed = (this.getTime() - this.currentTime)/1000;
+      this.timeElapsedMin = Math.floor(timeElapsed/60);
+      this.timeElapsedSec = (timeElapsed/60 - this.timeElapsedMin)*60;
+      let wordArray = this.randomText.split(" ");
+      //Need to adjust wordArray.length by the number of paragraphs besides the
+      //first, because the split function misses the paragraph start word as a
+      //separate word due to '\n' being the delimiter
+      this.wpm = (wordArray.length + this.randomTextArr.length-1)/(timeElapsed/60);
+
+      return true;
   }
 
   getInputText(e: any) {
@@ -38,7 +98,7 @@ export class AppComponent {
             e.target.value = this.inputText;
         }
       //only add one letter at a time!
-      else if(text.length === this.inputText.length+1) {
+      else if(text.length === this.inputText.length+1 || (e.target.value === '\n')) {
           this.inputText = text;
       }
       //allow multiple character deletions
@@ -52,9 +112,27 @@ export class AppComponent {
       return this.inputText;
   }
 
-  checkChar(char: string, i: number) {
-      if(i < this.inputText.length) {
-          return this.inputText[i] === char ? 'correct':'incorrect';
+  findDifference() {
+      let difference = ""
+      let i = 0;
+      while (i<this.randomText.length && i < this.inputText.length) {
+          if(this.randomText[i]!==this.inputText[i]) {
+              difference += this.randomText[i] + " vs. " + this.inputText[i] + " at " + i + '\n';
+          }
+          i++;
+      }
+      console.log(difference);
+  }
+
+  checkChar(char: string, i: number, j: number) {
+      let paras = 0;
+      let shift = 0;
+      while(paras<i) {
+          shift+=this.randomTextArr[paras].length;
+          paras++;
+      }
+      if(j + shift < this.inputText.length) {
+          return this.inputText[j + shift] === char ? 'correct':'incorrect';
       }
       else
       {
