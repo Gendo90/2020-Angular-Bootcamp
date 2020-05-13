27c9a5fb606294c60a94aa3142eec4407a59b591
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-05-13 15:03:55 -0700

    feat: Get "Fidget Spinner" app fully operational, just need to add some bells and whistles!

--- fidget-spinner/src/app/app.component.ts
+++ fidget-spinner/src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent {
   spinning = false;
   last_pos = null;
   last_ang = null
+  w_velocity = 0;
+  curr_timestamp = null;
+  spinning_int = null;
 
   start_Spin(event: any) {
       if(!this.center) {
@@ -23,6 +26,14 @@ export class AppComponent {
 
           this.spinner_img = document.querySelector(".spinner_image");
       }
+
+      if(this.spinning_int) {
+          clearInterval(this.spinning_int)
+          // console.log("SHOULD BE CLEARED!!!!")
+          this.w_velocity = 0;
+      }
+
+      this.curr_timestamp = event.timeStamp;
 
       console.log(event)
       console.log(this.center)
@@ -49,14 +60,60 @@ export class AppComponent {
           // console.log(this.last_ang)
           console.log(nextAng)
           // console.log(Math.atan((nextPos.y)/(nextPos.x)))
+          this.w_velocity = (nextAng-this.last_ang)/(event.timeStamp-this.curr_timestamp);
+          console.log(this.w_velocity)
+          this.curr_timestamp = event.timeStamp;
           this.last_pos = nextPos;
           this.last_ang = nextAng;
+      }
+  }
+
+  //included below as an arrow function, can put this in later
+  freespin() {
+      console.log(still_spinning)
+      if(Math.abs(this.w_velocity)>0.01) {
+          let nextAng = this.last_ang - this.w_velocity*(25)
+          this.spinner_img.style.transform = `rotate(${nextAng}deg)`;
+          this.last_ang = nextAng;
+          console.log("Omega is ", this.w_velocity)
+          this.w_velocity = this.w_velocity/1.01;
+      }
+      else {
+          // console.log(this.w_velocity)
+          clearInterval(this.spinning_int)
+          this.w_velocity = 0
+          // console.log(this.spinning_int)
+          // console.log("done")
       }
   }
 
   end_Spin(event: any) {
       if(this.spinning===true){
           this.spinning = false;
+          // console.log(Math.abs(this.w_velocity))
+          let root = this;
+          if(!Math.abs(this.w_velocity)<=0.01) {
+              console.log(this.spinning_int)
+              this.spinning_int = setInterval(() => {
+                  console.log("still_spinning")
+                  if(Math.abs(this.w_velocity)>0.01) {
+                      let nextAng = this.last_ang + this.w_velocity*(25)
+                      this.spinner_img.style.transform = `rotate(${nextAng}deg)`;
+                      this.last_ang = nextAng;
+                      console.log("Omega is ", this.w_velocity)
+                      this.w_velocity = this.w_velocity/1.01;
+                  }
+                  else {
+                      console.log(this.w_velocity)
+                      clearInterval(this.spinning_int)
+                      this.w_velocity = 0
+                      // console.log(this.spinning_int)
+                      // console.log("done")
+                  }
+              }, 25);
+              // console.log(this.spinning_int)
+              // console.log("free spinning")
+          }
       }
   }
 }
